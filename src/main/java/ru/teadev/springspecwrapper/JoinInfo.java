d48ad2318ae0991a5c824d9e89e11c504015f6a9
DIKovalenko <dikovalenko@dev.vtb.ru>
2022-06-24 17:41:13 +0400

    Add Type for JoinInfo

--- src/main/java/ru/teadev/springspecwrapper/JoinInfo.java
+++ src/main/java/ru/teadev/springspecwrapper/JoinInfo.java
@@ -1,34 +1,56 @@
 package ru.teadev.springspecwrapper;
 
+import javax.persistence.criteria.JoinType;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+
+import static org.hibernate.query.criteria.internal.path.AbstractFromImpl.DEFAULT_JOIN_TYPE;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-import javax.persistence.metamodel.ListAttribute;
-import javax.persistence.metamodel.SetAttribute;
-import javax.persistence.metamodel.SingularAttribute;
-
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public final class JoinInfo<F, J> {
 
+    @NonNull
     private final String attributeName;
+    @NonNull
+    private final JoinType type;
 
     public static <F, J> JoinInfo<F, J> create(@NonNull SingularAttribute<? super F, J> joinedAttr) {
-        return create(joinedAttr.getName());
+        return create(joinedAttr, DEFAULT_JOIN_TYPE);
     }
 
     public static <F, J> JoinInfo<F, J> create(@NonNull SetAttribute<? super F, J> joinedAttr) {
-        return create(joinedAttr.getName());
+        return create(joinedAttr, DEFAULT_JOIN_TYPE);
     }
 
     public static <F, J> JoinInfo<F, J> create(@NonNull ListAttribute<? super F, J> joinedAttr) {
-        return create(joinedAttr.getName());
+        return create(joinedAttr, DEFAULT_JOIN_TYPE);
     }
 
-    private static <F, J> JoinInfo<F, J> create(@NonNull String joinedAttrName) {
-        return new JoinInfo<>(joinedAttrName);
+    public static <F, J> JoinInfo<F, J> create(@NonNull SingularAttribute<? super F, J> joinedAttr,
+                                               @NonNull JoinType type) {
+        return create(joinedAttr.getName(), type);
+    }
+
+    public static <F, J> JoinInfo<F, J> create(@NonNull SetAttribute<? super F, J> joinedAttr,
+                                               @NonNull JoinType type) {
+        return create(joinedAttr.getName(), type);
+    }
+
+    public static <F, J> JoinInfo<F, J> create(@NonNull ListAttribute<? super F, J> joinedAttr,
+                                               @NonNull JoinType type) {
+        return create(joinedAttr.getName(), type);
+    }
+
+    private static <F, J> JoinInfo<F, J> create(@NonNull String joinedAttrName,
+                                                @NonNull JoinType type) {
+        return new JoinInfo<>(joinedAttrName, type);
     }
 
 }
